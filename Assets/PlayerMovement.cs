0a51c55f2b3e4aba540f4360b116725b865c85fa
Namit Singh <namit.s14@gmail.com>
2020-05-05 17:26:25 -0700

    Created project repo

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public Rigidbody2D rigidBody;
+    private float horizontalMove;
+    public float runSpeed;
+    private float verticalMove;
+    private bool jump;
+    public AudioClip jumpAudio;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        runSpeed = 25f;
+        horizontalMove = 0f;
+        jump = false;
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        verticalMove = rigidBody.velocity.y;
+        animator.SetFloat("horizontalSpeed", Mathf.Abs(horizontalMove));
+        animator.SetFloat("verticalSpeed", verticalMove);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("Jump", true);
+            AudioManager.Instance.Play(jumpAudio, transform);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Character Movement
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+        
+    }
+
+    public void isLanding()
+    {
+        animator.SetBool("Jump", false);
+    }
+}
---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+
+    public AudioClip clip;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        CharacterController2D player = collision.GetComponent<CharacterController2D>();
+        
+        if(player != null)
+        {
+            if (Input.GetButtonDown("Interact"))
+            {
+                AudioManager.Instance.Play(clip, player.transform);
+                Destroy(gameObject);
+            }
+            
+        }
+    }
+}
